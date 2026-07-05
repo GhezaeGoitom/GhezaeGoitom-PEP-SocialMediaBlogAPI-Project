2a8815beca4b53cbc5cc9a0732b27dcbc0686a0a
GhezaeGoitom <GhezaeGoitom@github.com>
2026-07-05 19:28:00 +0000

    Test run: 2/23 passed, 21 failed, 0 skipped - 2026-07-05T19:28:00.479Z

--- src/main/java/DAO/AccountDAO.java
+++ src/main/java/DAO/AccountDAO.java
@@ -11,7 +11,7 @@ import Util.ConnectionUtil;
 
 public class AccountDAO {
   
-String userRegistrationQuery = "INSERT INTO account(account_id,username,password) VALUES (?,?,?)";
+String userRegistrationQuery = "INSERT INTO account(username,password) VALUES (?,?)";
 String getUserByUserNameQuery = "SELECT * FROM account WHERE username = ?";
 
 
@@ -23,12 +23,17 @@ public Account userRegistration(Account account){
 try (Connection connection = ConnectionUtil.getConnection()) {
   
 PreparedStatement ps = connection.prepareStatement(userRegistrationQuery);
-ps.setInt(1, account.getAccount_id());
 ps.setString(2, account.getUsername());
 ps.setString(3, account.getPassword());
 
 
-ps.executeUpdate();
+ResultSet rs = ps.executeQuery();
+
+while (rs.next()) {
+  account.setAccount_id(rs.getInt("account_id"));
+  account.setUsername(rs.getString("username"));
+  account.setPassword(rs.getString("password"));
+}
 
 return account;
 } catch (Exception e) {
